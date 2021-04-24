5b502b6f0a2faf5f1e466c1705062c963aa52a80
Stephan Paukner <paux+dev@paukner.cc>
2021-04-24 22:18:45 +0200

    Added Swagger -- damn missing springfox-data-rest dependency cost me so much time :/

---
+++ src/main/java/cc/paukner/config/SwaggerConfig.java
@@ -0,0 +1,9 @@
+package cc.paukner.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+}
